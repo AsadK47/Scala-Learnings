d1b8c6bdbe5dd2ec70c93a93a94d8a9798aecc1c
Asad Khan <asad_khan47@hotmail.co.uk>
2020-06-03 19:58:26 +0100

    Quarter way finished with the exercises

--- src/lectures/part1basics/Functions.scala
+++ src/lectures/part1basics/Functions.scala
@@ -20,4 +20,36 @@ object Functions extends App {
   println(aRepeatedFunction("Hello", 3))
 
   // WHEN YOU NEED LOOPS, USE RECURSION.
+
+  def aFunctionWithSideEffects(aString: String): Unit = println(aString)
+
+  def aBigFunction(n: Int): Int = {
+    def aSmallerFunction(a: Int, b: Int): Int = a + b
+
+    aSmallerFunction(n, n - 1)
+  }
+
+  /*
+    1. A greeting function(name, age) => "Hi, my name is $name and I am $age years old"
+    2. Factorial function 1 * 2 * 3 * .. * n (recursive function)
+    3. A Fibonacci function
+       f(1) = 1
+       f(2) = 1
+       f(n) = f(n - 1) + f(n - 2)
+    4. Tests if a number is prime
+   */
+
+  def greetingFunction(name: String, age: Int): Unit = {
+    var message: String = "Hi, my name is " + name + " and I am " + age + " years old."
+    println(message)
+  }
+
+  greetingFunction("Asad", 25)
+
+  def factorialFunction(n: Int): Unit = {
+    if (n <= 2) return n
+    else n * factorialFunction(n - 1)
+  }
+
+  factorialFunction(3)
 }
--- src/playground/JavaPlayground.java
+++ src/playground/JavaPlayground.java
@@ -3,5 +3,44 @@ package playground;
 public class JavaPlayground {
     public static void main(String[] args) {
         System.out.println("Hello, Java");
+        factorialFunction(4);
     }
+
+    public static void factorialFunction(int n) {
+        long factorial = 1;
+        for (int i = 1; i <= n; i++) {
+            factorial *= i;
+        }
+        System.out.println(factorial);
+
+        /**
+         * factorial = 1
+         *
+         * Step 1:
+         * i = 1
+         * 1 <= 4 == true
+         * factorial = 1 * 1 == 1
+         *
+         * Step 2:
+         * i is incremented to 2
+         * 2 <= 4 == true
+         * factorial = 1 * 2 == 2
+         *
+         * Step 3:
+         * i is incremented to 3
+         * 3 <= 4 == true
+         * factorial = 2 * 3 == 6
+         *
+         * Step 4:
+         * i is incremented to 4
+         * 4 <= 4 == true
+         * factorial = 6 * 4 == 24
+         *
+         * Step 5:
+         * i is incremented to 5
+         * 5 <= 4 == false
+         * break
+         */
+    }
+
 }
