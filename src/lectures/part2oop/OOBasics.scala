26c0cfa52f6f2e1c33d4cc0fe7769cee716c454a
Asad Khan <asad_khan47@hotmail.co.uk>
2020-06-07 17:55:00 +0100

    Addition

--- src/lectures/part2oop/OOBasics.scala
+++ src/lectures/part2oop/OOBasics.scala
@@ -5,6 +5,12 @@ object OOBasics extends App {
   println(person.x)
   person.greet("Asad")
   person.greet()
+
+  val author = new Writer("Charles", "Dickens", 1812)
+  val imposter = new Writer("Charles", "Dickens", 1812)
+  val novel = new Novel("Great expectations", 1861, author)
+  println(novel.authorAge())
+  println(novel.isWrittenBy(author))
 }
 
 // Constructor
@@ -24,7 +30,49 @@ class Person(name: String, val age: Int = 0) {
 
   // Multiple constructors
   def this(name: String) = this(name, 0)
+
   def this() = this("John Doe")
 }
 
+/*
+  Novel and a Writer
+
+  Writer: first name, surname, year
+  - method fullname
+
+  Novel: name, year of release, author
+  - authorAge
+  - isWrittenBy(author)
+  - copy (new year of release) = new instance of Novel
+ */
+
+/*
+  Counter class
+  - receives an int value
+  - method current count
+  - method to increment / decrement => new Counter
+  - overload inc / dec to receive an amount
+
+ */
+
 // Class parameters are NOT FIELDS
+
+class Writer(firstName: String, surname: String, val year: Int) {
+  def fullName(): Unit = {
+    println(firstName + " " + surname)
+  }
+}
+
+class Novel(name: String, yearOfRelease: Int, author: Writer) {
+  def authorAge(): Unit = {
+    yearOfRelease - author.year
+  }
+
+  def isWrittenBy(author: Writer): Unit = {
+    author == this.author
+  }
+
+  def copy(newYearOfRelase: Int): Novel = {
+    new Novel(name, newYearOfRelase, author)
+  }
+}
