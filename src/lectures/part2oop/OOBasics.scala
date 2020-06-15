3b645bfd3beb4f0d1b488c1f14a2740dfb32d1f7
Asad Khan <asad_khan47@hotmail.co.uk>
2020-06-15 19:27:26 +0100

    OO Basics finishing

--- src/lectures/part2oop/OOBasics.scala
+++ src/lectures/part2oop/OOBasics.scala
@@ -9,8 +9,14 @@ object OOBasics extends App {
   val author = new Writer("Charles", "Dickens", 1812)
   val imposter = new Writer("Charles", "Dickens", 1812)
   val novel = new Novel("Great expectations", 1861, author)
+
   println(novel.authorAge())
-  println(novel.isWrittenBy(author))
+  println(novel.isWrittenBy(imposter))
+
+  val counter = new Counter()
+  counter.inc.print()
+  counter.inc.inc.inc.print()
+  counter.inc(10).print()
 }
 
 // Constructor
@@ -64,15 +70,39 @@ class Writer(firstName: String, surname: String, val year: Int) {
 }
 
 class Novel(name: String, yearOfRelease: Int, author: Writer) {
-  def authorAge(): Unit = {
+  def authorAge(): Int = {
     yearOfRelease - author.year
   }
 
-  def isWrittenBy(author: Writer): Unit = {
+  def isWrittenBy(author: Writer): Boolean = {
     author == this.author
   }
 
-  def copy(newYearOfRelase: Int): Novel = {
-    new Novel(name, newYearOfRelase, author)
+  def copy(newYearOfRelease: Int): Novel = {
+    new Novel(name, newYearOfRelease, author)
   }
+}
+
+class Counter(val count: Int = 0) {
+  def inc = {
+    println("incrementing")
+    new Counter(count + 1) // immutability
+  }
+
+  def dec = {
+    println("decrementing")
+    new Counter(count - 1)
+  }
+
+  def inc(n: Int): Counter = {
+    if (n <= 0) this
+    else inc.inc(n - 1)
+  }
+
+  def dec(n: Int): Counter = {
+    if (n <= 0) this
+    else dec.dec(n - 1)
+  }
+
+  def print(): Unit = println(count)
 }
