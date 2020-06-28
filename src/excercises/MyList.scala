a95c1a5d2a567a324a8990c03e6dd3799beb66c1
Asad Khan <asad_khan47@hotmail.co.uk>
2020-06-28 14:14:24 +0100

    Removing exercises folder from gitignore

---
+++ src/excercises/MyList.scala
@@ -0,0 +1,14 @@
+package excercises
+
+abstract class MyList {
+  /*
+      head = first element of the list
+      tail = remainder of the list
+      isEmpty = is this list empty
+      add(int) => new list with this element added
+      toString => a string representation of the list
+   */
+  val list: List[Int] = List(1, 2, 3)
+
+
+}
