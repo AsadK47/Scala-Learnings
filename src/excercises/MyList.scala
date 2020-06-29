2fe360fae80f9705214ca2f84823196174faa6e9
Asad Khan <asad_khan47@hotmail.co.uk>
2020-06-29 19:51:59 +0100

    Generics in Scala

--- src/excercises/MyList.scala
+++ src/excercises/MyList.scala
@@ -31,29 +31,29 @@ package excercises
 //  }
 //}
 
-abstract class MyList {
-  def head: Int
-  def tail: MyList
+abstract class MyList[+A] {
+  def head: A
+  def tail: MyList[A]
   def isEmpty: Boolean
-  def add(element: Int): MyList
+  def add[B >: A](element: B): MyList[B]
   def printElements: String
   // polymorphic call
   override def toString: String = "[" + printElements + "]"
 }
 
-object Empty extends MyList {
-  override def head: Int = throw new NoSuchElementException
-  override def tail: MyList = throw new NoSuchElementException
+object Empty extends MyList[Nothing] {
+  override def head: Nothing = throw new NoSuchElementException
+  override def tail: MyList[Nothing] = throw new NoSuchElementException
   override def isEmpty: Boolean = true
-  override def add(element: Int): MyList = new Cons(element, Empty)
-  override def printElements: String = ???
+  override def add[B >: Nothing](element: B): MyList[B] = new Cons(element, Empty)
+  override def printElements: String = ""
 }
 
-class Cons(h: Int, t: MyList) extends MyList {
-  override def head: Int = h
-  override def tail: MyList = t
+class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
+  override def head: A = h
+  override def tail: MyList[A] = t
   override def isEmpty: Boolean = false
-  override def add(element: Int): MyList = new Cons(element, this)
+  override def add[B >: A](element: B): MyList[B] = new Cons(element, this)
 
   override def printElements: String = {
     if (t.isEmpty) "" + head
@@ -62,10 +62,9 @@ class Cons(h: Int, t: MyList) extends MyList {
 }
 
 object ListTest extends App {
-  val list = new Cons(1, new Cons(2, new Cons(3, Empty)))
-  println(list.tail.head)
-  println(list.add(4).head)
-  println(list.isEmpty)
+  val listOfIntegers: MyList[Int] = new Cons[Int](1, new Cons(2, new Cons(3, Empty)))
+  val listOfStrings: MyList[String] = new Cons[String]("Hello", new Cons("Scala", Empty))
 
-  println(list.toString)
+  println(listOfIntegers.toString)
+  println(listOfStrings.toString)
 }
